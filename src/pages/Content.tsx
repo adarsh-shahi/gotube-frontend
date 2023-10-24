ac5cf8b04289e2f55be73e676c18ba9c78b1ad3f
adarsh-shahi <adarshshahi1009@gmail.com>
2023-10-24 23:30:32 +0530

    s3 meta-data key

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { ChangeEvent, useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/UserContext";
 
 interface IContentType {
@@ -20,7 +20,46 @@ export default function Content() {
 	const [title, setTitle] = useState("");
 	const [description, setDescription] = useState("");
 	const [tags, setTags] = useState("");
-	const [file, setFile] = useState("");
+	const [file, setFile] = useState<null | File>(null);
+
+	const [ownerId, setOwnerId] = useState("");
+	const [contentId, setContentId] = useState("");
+
+	const handleSaveChanges = async () => {
+		console.log(file);
+		console.log(typeof file);
+
+		try {
+			// send meta data to server and get pre signed urls to upload to s3
+			const signedUrlResponse = await fetch(
+				`http://localhost:8001/video?ownerId=${ownerId}&contentId=${contentId}`,
+				{
+					method: "put",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${state.user.jwtToken}`,
+					},
+					body: JSON.stringify({
+						type: file?.type.split("/")[1],
+						size: file?.size,
+					}),
+				}
+			);
+			const { data } = await signedUrlResponse.json();
+			const { url, key } = data;
+			const s3Upload = await fetch(url, {
+				method: "put",
+				body: file,
+			});
+			await s3Upload.json();
+
+			//save key to DB
+		} catch (err) {
+			console.log(err);
+		}
+
+		// after successfull uploading send message to server
+	};
 
 	useEffect(() => {
 		const contendId =
@@ -31,6 +70,8 @@ export default function Content() {
 			window.location.href.split("/")[
 				window.location.href.split("/").length - 3
 			];
+		setContentId(contendId);
+		setOwnerId(ownerId);
 		(async () => {
 			const response = await fetch(
 				`http://localhost:8001/content?ownerId=${ownerId}&contentId=${contendId}`,
@@ -44,7 +85,7 @@ export default function Content() {
 			if (error) return;
 			console.log(data);
 		})();
-	});
+	}, []);
 
 	return (
 		<div className="m-5 border-2 border-gray-600 rounded-2xl grow grid grid-cols-2 grid-rows-6 p-4 gap-5 relative">
@@ -53,16 +94,23 @@ export default function Content() {
 				className="h-min border-2 border-gray-600 px-2 py-1 text-2xl rounded-xl row-span-1"
 				type="text"
 				placeholder="Title"
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
 			/>
 			<input
 				className="h-min border-2 border-gray-600 px-2 py-1 text-2xl rounded-xl row-span-3"
 				type="file"
 				placeholder="Video File"
+				onChange={(e: ChangeEvent<HTMLInputElement>) => {
+					setFile(e.target!.files![0]);
+				}}
 			/>
 			<input
 				className="h-min border-2 border-gray-600 px-2 py-1 text-2xl rounded-xl"
 				type="text"
 				placeholder="Description"
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
 			/>
 			<input
 				className="h-min border-2 border-gray-600 px-2 py-1 text-2xl rounded-xl"
@@ -70,9 +118,14 @@ export default function Content() {
 				placeholder="Tags"
 				width="100px"
 				height="100px"
+				value={tags}
+				onChange={(e) => setTags(e.target.value)}
 			/>
 			<div className="absolute right-10 bottom-10 flex gap-5">
-				<button className="bg-green-500 text-white px-3 py-1 font-semibold text-lg rounded-2xl">
+				<button
+					className="bg-green-500 text-white px-3 py-1 font-semibold text-lg rounded-2xl"
+					onClick={handleSaveChanges}
+				>
 					Save Changes
 				</button>
 				{state.user.uType === "owner" && (
